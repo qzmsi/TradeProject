9674386b570c18fd3ef8069ac1f2a216e6d619e9
qzmsi <wdy203522986970@163.com>
2021-06-03 19:53:17 +0800

    add the orders page and the link to the orderpage

--- seller/js/custom/dashboard.js
+++ seller/js/custom/dashboard.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
-	
+
 		jQuery('#phone').click(function(){
 			jQuery('.centercontent iframe').attr("src","phone.html");
 		});
@@ -9,7 +9,13 @@ jQuery(document).ready(function(){
 		jQuery('#firstPage').click(function(){
 			jQuery('.centercontent iframe').attr("src","first.html");
 		});
-		
+		jQuery('#phoneorder').click(function() {
+			jQuery('.centercontent iframe').attr("src","phoneorder.html");
+		})
+		jQuery('#bedorder').click(function() {
+			jQuery('.centercontent iframe').attr("src","bedorder.html");
+		})
+
 		jQuery("#productManagementBtn").click(function(){
 			jQuery("#productManagement").show();
 			jQuery("#orderManagement").hide();
@@ -18,27 +24,27 @@ jQuery(document).ready(function(){
 			jQuery("#orderManagement").show();
 			jQuery("#productManagement").hide();
 		})
-	
+
 		//jQuery('#overviewselect, input:checkbox').uniform();
-		
+
 		///// DATE PICKER /////
 		//jQuery( "#datepickfrom, #datepickto" ).datepicker();
-		
+
 		///// SLIM SCROLL /////
 		jQuery('#scroll1').slimscroll({
 			color: '#666',
 			size: '10px',
 			width: 'auto',
-			height: '175px'                  
+			height: '175px'
 		});
-		
+
 		///// ACCORDION /////
 		jQuery('#accordion').accordion({autoHeight:  false});
-	
+
 		///// SIMPLE CHART /////
 		var flash = [[0, 2], [1, 6], [2,3], [3, 8], [4, 5], [5, 13], [6, 8]];
 		var html5 = [[0, 5], [1, 4], [2,4], [3, 1], [4, 9], [5, 10], [6, 13]];
-			
+
 		function showTooltip(x, y, contents) {
 			jQuery('<div id="tooltip" class="tooltipflot">' + contents + '</div>').css( {
 				position: 'absolute',
@@ -47,8 +53,8 @@ jQuery(document).ready(function(){
 				left: x + 5
 			}).appendTo("body").fadeIn(200);
 		}
-	
-			
+
+
 		var plot = jQuery.plot(jQuery("#chartplace"),
 			   [ { data: flash, label: "Flash(x)", color: "#069"}, { data: html5, label: "HTML5(x)", color: "#FF6600"} ], {
 				   series: {
@@ -59,39 +65,39 @@ jQuery(document).ready(function(){
 				   grid: { hoverable: true, clickable: true, borderColor: '#ccc', borderWidth: 1, labelMargin: 10 },
 				   yaxis: { min: 0, max: 15 }
 				 });
-		
+
 		var previousPoint = null;
 		jQuery("#chartplace").bind("plothover", function (event, pos, item) {
 			jQuery("#x").text(pos.x.toFixed(2));
 			jQuery("#y").text(pos.y.toFixed(2));
-			
+
 			if(item) {
 				if (previousPoint != item.dataIndex) {
 					previousPoint = item.dataIndex;
-						
+
 					jQuery("#tooltip").remove();
 					var x = item.datapoint[0].toFixed(2),
 					y = item.datapoint[1].toFixed(2);
-						
+
 					showTooltip(item.pageX, item.pageY,
 									item.series.label + " of " + x + " = " + y);
 				}
-			
+
 			} else {
 			   jQuery("#tooltip").remove();
-			   previousPoint = null;            
+			   previousPoint = null;
 			}
-		
+
 		});
-		
+
 		jQuery("#chartplace").bind("plotclick", function (event, pos, item) {
 			if (item) {
 				jQuery("#clickdata").text("You clicked point " + item.dataIndex + " in " + item.series.label + ".");
 				plot.highlight(item.series, item.datapoint);
 			}
 		});
-		
-		
+
+
 	///// SWITCHING LIST FROM 3 COLUMNS TO 2 COLUMN LIST /////
 	function rearrangeShortcuts() {
 		if(jQuery(window).width() < 430) {
@@ -102,7 +108,7 @@ jQuery(document).ready(function(){
 					jQuery(this).addClass('one_half');
 					if(count%2 != 0) jQuery(this).addClass('last');
 					count++;
-				});	
+				});
 			}
 		} else {
 			if(jQuery('.shortcuts li.one_half').length > 0) {
@@ -110,9 +116,9 @@ jQuery(document).ready(function(){
 			}
 		}
 	}
-	
+
 	rearrangeShortcuts();
-	
+
 	///// ON RESIZE WINDOW /////
 	jQuery(window).resize(function(){
 		rearrangeShortcuts();
